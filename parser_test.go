a766c27a39857e591f531b74f120a0862e505e65
Michael Tiller <michael.tiller@gmail.com>
2014-10-26 10:05:55 -0400

    Refining testing

--- parser_test.go
+++ parser_test.go
@@ -23,36 +23,6 @@ computer XYZ {
 }
 `
 
-func Test_LexerStream(t *testing.T) {
-	RegisterTestingT(t)
-
-	var lval yySymType
-	r := strings.NewReader("set x = 5;")
-	lexer := NewLexer(r)
-	tok1 := lexer.Lex(&lval)
-
-	Expect(tok1).To(Equal(IDENTIFIER))
-	Expect(lval.identifier).To(Equal("set"))
-
-	tok2 := lexer.Lex(&lval)
-
-	Expect(tok2).To(Equal(IDENTIFIER))
-	Expect(lval.identifier).To(Equal("x"))
-
-	tok3 := lexer.Lex(&lval)
-
-	Expect(tok3).To(Equal(EQUALS))
-
-	tok4 := lexer.Lex(&lval)
-
-	Expect(tok4).To(Equal(NUMBER))
-	Expect(lval.number).To(Equal(5))
-
-	tok5 := lexer.Lex(&lval)
-
-	Expect(tok5).To(Equal(SEMI))
-}
-
 func Test_SimpleDeclaration(t *testing.T) {
 	RegisterTestingT(t)
 
