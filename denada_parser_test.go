8e406b25705a5d0c07a8189274bb6f109d5e5f31
Michael Tiller <michael.tiller@gmail.com>
2014-10-26 09:49:49 -0400

    Switching to public testing framework

--- denada_parser_test.go
+++ denada_parser_test.go
@@ -1,10 +1,9 @@
 package denada
 
 import "testing"
-import "log"
 import "strings"
 
-import . "github.com/xogeny/gocore/xassert"
+import . "github.com/onsi/gomega"
 
 var sample = `
 printer 'ABC' {
@@ -25,48 +24,61 @@ computer XYZ {
 `
 
 func Test_LexerStream(t *testing.T) {
-	ExtraLayer(t, "Testing lexer", func() {
-		var lval yySymType
-		r := strings.NewReader("set x = 5;")
-		lexer := NewLexer(r)
-		tok1 := lexer.Lex(&lval)
-		IsEqual(tok1, IDENTIFIER)
-		IsEqual(lval.identifier, "set")
-		tok2 := lexer.Lex(&lval)
-		IsEqual(tok2, IDENTIFIER)
-		IsEqual(lval.identifier, "x")
-		tok3 := lexer.Lex(&lval)
-		IsEqual(tok3, EQUALS)
-		tok4 := lexer.Lex(&lval)
-		IsEqual(tok4, NUMBER)
-		tok5 := lexer.Lex(&lval)
-		IsEqual(tok5, SEMI)
-	})
+	RegisterTestingT(t)
+
+	var lval yySymType
+	r := strings.NewReader("set x = 5;")
+	lexer := NewLexer(r)
+	tok1 := lexer.Lex(&lval)
+
+	Expect(tok1).To(Equal(IDENTIFIER))
+	Expect(lval.identifier).To(Equal("set"))
+
+	tok2 := lexer.Lex(&lval)
+
+	Expect(tok2).To(Equal(IDENTIFIER))
+	Expect(lval.identifier).To(Equal("x"))
+
+	tok3 := lexer.Lex(&lval)
+
+	Expect(tok3).To(Equal(EQUALS))
+
+	tok4 := lexer.Lex(&lval)
+
+	Expect(tok4).To(Equal(NUMBER))
+	Expect(lval.number).To(Equal(5))
+
+	tok5 := lexer.Lex(&lval)
+
+	Expect(tok5).To(Equal(SEMI))
 }
 
 func Test_SimpleDeclaration(t *testing.T) {
-	ExtraLayer(t, "Testing parser", func() {
-		r := strings.NewReader("set x = 5 \"Description\";")
-		elems, err := Parse(r)
-		NoError(err)
-		IsEqual(1, len(elems))
-		elem := elems[0]
-		log.Printf("Elem = %v", elem)
-		IsTrue(elem.isDeclaration())
-		IsFalse(elem.isDefinition())
-		IsEqual(0, len(elem.Modifications))
-		Resembles(elem.Qualifiers, []string{"set"})
-		IsEqual(elem.Name, "x")
-		IsEqual(elem.Description, "Description")
-		Resembles(elem.Value, 5)
-	})
+	RegisterTestingT(t)
+
+	r := strings.NewReader("set x = 5 \"Description\";")
+	elems, err := Parse(r)
+
+	Expect(err).To(BeNil())
+	Expect(len(elems)).To(Equal(1))
+
+	elem := elems[0]
+
+	Expect(elem.isDeclaration()).To(BeTrue())
+	Expect(elem.isDefinition()).To(BeFalse())
+	Expect(len(elem.Modifications)).To(Equal(0))
+
+	Expect(elem.Qualifiers).To(Equal([]string{"set"}))
+	Expect(elem.Name).To(Equal("x"))
+	Expect(elem.Description).To(Equal("Description"))
+	Expect(elem.Value).To(Equal(5))
 }
 
 func Test_SampleInput(t *testing.T) {
-	ExtraLayer(t, "Testing parser", func() {
-		r := strings.NewReader(sample)
-		elems, err := Parse(r)
-		NoError(err)
-		IsEqual(3, len(elems))
-	})
+	RegisterTestingT(t)
+
+	r := strings.NewReader(sample)
+	elems, err := Parse(r)
+	Expect(err).To(BeNil())
+	Expect(len(elems)).To(Equal(3))
 }
